68edc261db955c59db64f7d835e44f34bad90ba2
Esalamancaa <esteban.salamanca.aravena@gmail.com>
2025-04-29 17:16:55 -0400

    29-04-2025

--- TareaCajeroAutomático.py
+++ TareaCajeroAutomático.py
@@ -1,23 +1,15 @@
 import time
 
 #Usuarios
-user1="carlos"
-user2="juan"
-user3="jaime"
+user1="carlos" ; user2="juan" ; user3="jaime"
 #Contraseñas
-c1=9053
-c2=6700
-c3=2965
+c1=9053 ; c2=6700 ; c3=2965
 #Saldo
-s1=80000
-s2=20000
-s3=50000
+s1=80000 ; s2=20000 ; s3=50000
 #Contador inicial de intentos
 intentos=2
 #Cajas de billetes
-cj1=30 #$5000
-cj2=30 #10000
-cj3=30 #20000
+cj1=30 ; cj2=30 ; cj3=30 
 #Inicio de programa
 print("Ingrese su usuario :")
 user=input()
@@ -51,6 +43,8 @@ if user.lower()==user1 or user2 or user3:
             cc=int(input("Ingrese la contraseña :"))
             if intentos==0:
                 print("Numero de intentos excedido, la cuenta se ha bloqueado.")
+    else:
+        user=int(input("Ingrese un usuario válido :"))                
              
 time.sleep(1)
 
@@ -66,47 +60,45 @@ op=int(input())
 #Validación de elección y disponibilidad de billetes
 while op!=4:
     if op==1:
-        if cj1>0:
-            print("Se retirarán $5000 de su saldo")
+        if cj1>0 and saldo>=5000:
             cj2-=1
             saldo-=5000
-            print(f"Su saldo disponible es {saldo}")
-            print('''
+            print(f"Se retirarán $5000 de su saldo. Su saldo disponible es {saldo}")
+            op=int(input('''
                 ¿Cuanto más va a retirar?  
                 1.- $5000    2.- $10000
                 3.- $20000   4.- Salir 
-                  ''')
-            op=int(input())
+                  '''))
         else:
-            print("Este cajero se ha quedado sin billetes de $10000") 
+            op=int(input("Este cajero se ha quedado sin billetes de $5000 o no tiene suficiente saldo. Ingrese otra opción :"))
     elif op==2:
-        if cj2>0:
-            print("Se retirarán $10000 de su saldo")
+        if cj2>0 and saldo>=10000:
             cj2-=1
             saldo-=10000
-            print(f"Su saldo disponible es {saldo}")
-            print('''
+            print(f"Se retirarán $10000 de su saldo. Su saldo disponible es {saldo}")
+            op=int(input('''
                 ¿Cuanto más va a retirar?  
                 1.- $5000    2.- $10000
                 3.- $20000   4.- Salir 
-                  ''')
-            op=int(input())
-        else:
-            print("Este cajero se ha quedado sin billetes de $10000")    
+                  '''))
+        else:  
+            op=int(input("Este cajero se ha quedado sin billetes de $10000 o no tiene suficiente saldo. Ingrese otra opción :"))  
     elif op==3:
-        if cj3>0:
-            print("Se retirarán $20000 de su saldo")
+        if cj3>0 and saldo>=20000:
             cj3-=1
-            saldo-=s3
-            print(f"Su saldo disponible es {saldo}")
-            print('''
+            saldo-=20000
+            print(f"Se retirarán $20000 de su saldo. Su saldo disponible es {saldo}")
+            op=int(input('''
                 ¿Cuanto más va a retirar?  
                 1.- $5000    2.- $10000
                 3.- $20000   4.- Salir 
-                  ''')
-            op=int(input())
+                  '''))
         else:
-            print("Este cajero se ha quedado sin billetes de $10000")               
+            op=int(input("Este cajero se ha quedado sin billetes de $20000 o no tiene suficiente saldo. Ingrese otra opción :"))            
     elif op==4:
         print("No se ha realizado ninguna operación. saliendo...")   
+    else:
+        op=int(input("Ingrese una opción válida para continuar :"))    
+
+
  
