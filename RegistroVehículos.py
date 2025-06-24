872cdf9316ce757b58fbed726a68006d69b5d6f9
Esalamancaa <esteban.salamanca.aravena@gmail.com>
2025-06-23 21:20:45 -0400

    Add files via upload

--- RegistroVehículos.py
+++ RegistroVehículos.py
@@ -1,12 +1,12 @@
 #Tarea
-#Crear gestión de vehículos
+#Crear gestión de Vehiculos
 #Crear programa para un parking de autos
 #Se debe ingresar marca, año, patente y tipo
 #Marca: Tipo string, debe typear la marca
 #Año: Solo de 4 digitos enteros
 #Patente: Debe tener 4 letras minusculas y 2 digitos
 #Tipo: S=sedan, C=Camioneta, M=Moto
-#Se debe validar cada aspecto y debe tener un mantenedor para todo los vehículos motorizados
+#Se debe validar cada aspecto y debe tener un mantenedor para todo los Vehiculos motorizados
 
 #1.- Ingresar vehiculos
 #2.- Mostrar vehiculos
@@ -17,131 +17,21 @@
 
 #Usar funciones con argumentos para poder validar y para poder llamar las acciones dentro del menú  
 
+va=0
+vb=0
 
- 
 def mostrar_vehiculos():
-    for key,vehiculo in Vehículos.items():
-        print(key,".-",vehiculo)
-def ingresar_vehiculos():
-    marca=input("Ingrese la marca: ")
-    while True:
-        año=int(input("Ingrese el año: "))
-        laño=str(año)
-        if len(laño)==4:
-            break
-        else:
-            print("El año debe estar en formato de 4 numeros. Ejemplo: 2010") 
-    while True:
-        patente=input("Ingrese la patente: ")
-        if len(patente)==6:
-            break
-        else:
-            print("La patente debe estar en formato de 4 letras y 2 números. Ejemplo: PLDE55") 
-    while True:             
-        tipo=input("¿Cuál es el tipo del vehículo?(S=sedan, C=Camioneta, M=Moto): ")
-        if tipo.upper()=="S" or tipo.upper()=="C" or tipo.upper()=="M":
-            break
-        else:
-            print("Ingrese una opción válida")
-    VehículosLargo=list(Vehículos.keys())[-1]
-    Vehículos[VehículosLargo+1]={
-                                "Marca":marca,
-                                "Año":año,
-                                "Patente":patente,
-                                "Tipo":tipo
-                                }  
-def actualizar_vehiculos():
-    mostrar_vehiculos()
-    vh=int(input("Ingrese que vehículo quiere actualizar"))
-    while True:
-        print(Vehículos[vh])
-        print('''
-            1.- Marca
-            2.- Año
-            3.- Patente 
-            4.- Tipo      
-            5.- Salir  
-            ''')
-        op2=int(input("Ingrese qué cambio quiere realizar: "))    
-        match op2:
-            case 1:
-                marca=input("Ingrese la marca: ") 
-                Vehículos[vh]["Marca"]=marca
-            case 2:
-                while True:
-                    año=int(input("Ingrese el año: "))
-                    laño=str(año)
-                    if len(laño)==4:
-                        break
-                    else:
-                        print("El año debe estar en formato de 4 numeros. Ejemplo: 2010") 
-                Vehículos[vh]["Año"]=año
-            case 3:
-                while True:
-                    patente=input("Ingrese la patente: ")
-                    if len(patente)==6 and patente.isalnum()==True:
-                        break
-                    else:
-                        print("La patente debe estar en formato de 4 letras y 2 números. Ejemplo: PLDE55") 
-                Vehículos[vh]["Patente"]=patente
-            case 4:
-                while True:             
-                    tipo=input("¿Cuál es el tipo del vehículo?(S=sedan, C=Camioneta, M=Moto): ")
-                    if tipo.upper()=="S" or tipo.upper()=="C" or tipo.upper()=="M":
-                        break
-                    else:
-                        print("Ingrese una opción válida")
-                Vehículos[vh]["Tipo"]=tipo       
-            case 5:
-                break
-            case _:
-                print("Ingrese una opción válida") 
-def borrar_vehiculos():
-    borrados=int(0)
-    mostrar_vehiculos()
-    delete=int(input("¿Qué vehículo quiere eliminar?"))  
-    del Vehículos[delete]
+    for key,vehiculo in Vehiculos.items():
+        print(key,".-",vehiculo)   
 def mostrar_estadisticas():
-    VL=list(Vehículos.keys())[-1]
+    VL=list(Vehiculos.keys())[-1]
     print(f'''
         Estadísticas:
-        Vehículo agregado recientemente: 
-        Cantidad de vehículos en garage: {VL} 
+        Vehículo agregado recientemente: {VL}
+        Cantidad de Vehiculos en garage: {2+(va-vb)} 
             ''')      
-def menu_de_registro():
-    while True:
-        while True:
-            try:
-                print('''
-    Ingrese la acción a realizar:
-    1.- Ingresar vehículos
-    2.- Mostrar vehiculos
-    3.- Actualizar vehiculo
-    4.- Borrar vehiculo
-    5.- Mostrar estadisticas: ultimo vehiculo ingresado y total en garage
-    6.- Salir
-    ''')
-                op=int(input())
-                break
-            except Exception as e:
-                print(e)    
-        match op:
-            case 1:
-                ingresar_vehiculos() 
-            case 2:
-                mostrar_vehiculos()
-            case 3:
-                actualizar_vehiculos()
-            case 4:
-                borrar_vehiculos()
-            case 5:
-                mostrar_estadisticas()                       
-            case 6:
-                print("Saliendo...")
-                break
-            case _:
-                print("Ingrese una opción válida")
-Vehículos={
+
+Vehiculos={
     1:{
     "Marca":"Chevrolet",
     "Año":2020,
@@ -156,4 +46,121 @@ Vehículos={
     }
 }
 
-menu_de_registro()
+while True:
+        while True:
+            try:
+                print('''
+                Ingrese la acción a realizar:
+                1.- Ingresar Vehiculos
+                2.- Mostrar vehiculos
+                3.- Actualizar vehiculo
+                4.- Borrar vehiculo
+                5.- Mostrar estadisticas: ultimo vehiculo ingresado y total en garage
+                6.- Salir
+                ''')
+                op=int(input())
+                break
+            except Exception as e:
+                print(e)    
+        match op:
+            case 1:
+                va+=1
+                marca=input("Ingrese la marca: ")
+                while True:
+                    anio=int(input("Ingrese el año del vehículo"))
+                    if len(str(anio))==4:
+                        break
+                    else:
+                        print("El año debe estar en formato de 4 numeros. Ejemplo: 2010") 
+                while True:
+                    patente=input("Ingrese la patente: ")
+                    cont_letras=0
+                    cont_numeros=0
+                    for l in patente:
+                        if l.isalpha():
+                            cont_letras+=1
+                        elif l.isdigit():
+                            cont_numeros+=1    
+                    if len(patente)==6 and cont_letras==4 and cont_numeros==2:
+                        break
+                    else:
+                        print("La patente debe estar en formato de 4 letras y 2 números. Ejemplo: PLDE55") 
+                while True:             
+                    tipo=input("¿Cuál es el tipo del vehículo?(S=sedan, C=Camioneta, M=Moto): ")
+                    if tipo.upper()=="S" or tipo.upper()=="C" or tipo.upper()=="M":
+                        break
+                    else:
+                        print("Ingrese una opción válida")
+                VehiculosLargo=list(Vehiculos.keys())[-1]
+                Vehiculos[VehiculosLargo+1]={
+                                            "Marca":marca,
+                                            "Año":anio,
+                                            "Patente":patente,
+                                            "Tipo":tipo
+                                            }  
+            case 2:
+                mostrar_vehiculos() 
+            case 3:
+                mostrar_vehiculos()
+                vh=int(input("Ingrese que vehículo quiere actualizar"))
+                while True:
+                    print(Vehiculos[vh])
+                    print('''
+                        1.- Marca
+                        2.- Año
+                        3.- Patente 
+                        4.- Tipo      
+                        5.- Salir  
+                        ''')
+                    op2=int(input("Ingrese qué cambio quiere realizar: "))    
+                    match op2:
+                        case 1:
+                            marca=input("Ingrese la marca: ") 
+                            Vehiculos[vh]["Marca"]=marca
+                        case 2:
+                            while True:
+                                año=int(input("Ingrese el año: "))
+                                if len(str(año))==4:
+                                    break
+                                else:
+                                    print("El año debe estar en formato de 4 numeros. Ejemplo: 2010") 
+                            Vehiculos[vh]["Año"]=anio
+                        case 3:
+                            while True:
+                                patente=input("Ingrese la patente: ")
+                                cont_letras=0
+                                cont_numeros=0
+                                for l in patente:
+                                    if l.isalpha():
+                                        cont_letras+=1
+                                    elif l.isdigit():
+                                        cont_numeros+=1    
+                                if len(patente)==6 and cont_letras==4 and cont_numeros==2:
+                                    break
+                                else:
+                                    print("La patente debe estar en formato de 4 letras y 2 números. Ejemplo: PLDE55") 
+                            Vehiculos[vh]["Patente"]=patente
+                        case 4:
+                            while True:             
+                                tipo=input("¿Cuál es el tipo del vehículo?(S=sedan, C=Camioneta, M=Moto): ")
+                                if tipo.upper()=="S" or tipo.upper()=="C" or tipo.upper()=="M":
+                                    break
+                                else:
+                                    print("Ingrese una opción válida")
+                            Vehiculos[vh]["Tipo"]=tipo       
+                        case 5:
+                            break
+                        case _:
+                            print("Ingrese una opción válida")
+            case 4:
+                mostrar_vehiculos()
+                delete=int(input("¿Qué vehículo quiere eliminar?"))  
+                del Vehiculos[delete]
+                vb+=1 
+            case 5:
+                mostrar_estadisticas()                     
+            case 6:
+                print("Saliendo...")
+                break
+            case _:
+                print("Ingrese una opción válida")
